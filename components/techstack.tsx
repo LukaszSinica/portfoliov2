8b4b37b1b093a56d159eaeb66f1ce4e17d3903e6
Lucas Sinica <64593019+LukaszSinica@users.noreply.github.com>
2025-06-28 15:19:44 +0200

    Update techstack.tsx

--- components/techstack.tsx
+++ components/techstack.tsx
@@ -9,7 +9,7 @@ export default function TechStack() {
       <span className='title text-2xl sm:text-3xl'>
         <Title name={'tech.title'} level='h2'/>
       </span>
-      <div className="flex flex-col sm:flex-row gap-6 min-h-[200px]">
+      <div className="flex flex-col md:flex-row gap-6 min-h-[200px]">
         <TechstackSide />
         <TechstackMain />
       </div>
